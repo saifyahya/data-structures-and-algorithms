27b0d2229032f694281bae2ca2db0e2524ba8783
saifyahya <saifbarakat991@gmail.com>
2023-11-20 20:03:19 +0000

    bfs

--- CC35/app/src/main/java/cc35/Graph.java
+++ CC35/app/src/main/java/cc35/Graph.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<T>>  // just in case you have Comparable data structures
 {
-    public HashMap<Vertex<T>, LinkedList<Edge<T>>> adjacencyLists;
+    public HashMap<T, List<Edge<T>>> adjacencyLists;
     private int numberOfVertices = 0;
 
     public Graph(int maxNumberOfVertices)
@@ -16,7 +16,7 @@ public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<
     {
         // TODO: implement me
         Vertex<T> newVertex = new Vertex<>(value);
-        adjacencyLists.put(newVertex,new LinkedList<>());
+        adjacencyLists.put(newVertex.value,new ArrayList<>());
         numberOfVertices++;
         return newVertex;
     }
@@ -28,28 +28,28 @@ public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<
 
     public void addEdge(Vertex<T> start, Vertex<T> destination, int weight) {
         // Check if both start and destination vertices are in the graph
-        if (adjacencyLists.containsKey(start) && adjacencyLists.containsKey(destination)) {
+        if (adjacencyLists.containsKey(start.value) && adjacencyLists.containsKey(destination.value)) {
             // Create edges with the destination vertex and the specified weight
             Edge<T> edge1 = new Edge<>(destination, weight);
             Edge<T> edge2 = new Edge<>(start, weight);
 
             // Add edges to the adjacency lists
-            adjacencyLists.get(start).add(edge1);
-             adjacencyLists.get(destination).add(edge2);
+            adjacencyLists.get(start.value).add(edge1);
+             adjacencyLists.get(destination.value).add(edge2);
         } else {
             System.out.println("One or both vertices are not present in the adjacencyLists map.");
         }
     }
 
 
-    public Set<Vertex<T>> getVertices()  // getNodes()
+    public Set<T> getVertices()  // getNodes()
     {
         // TODO: implement me
         return  adjacencyLists.keySet();
 
     }
 
-    public LinkedList<Edge<T>> getNeighbors(Vertex<T> vertex)
+    public List<Edge<T>> getNeighbors(T vertex)
     {
  return adjacencyLists.get(vertex);
     }
@@ -72,8 +72,8 @@ public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<
         // WARNING: Your HashMap does not guarantee ordering!
         // But you can basically rely on it if you add nodes to your graphs in a consistent way
         StringBuilder stringBuilder =new StringBuilder();
-        for (Map.Entry<Vertex<T>,LinkedList<Edge<T>>> vertex:adjacencyLists.entrySet()){
-            stringBuilder.append( "Vertex: "+vertex.getKey().value+" Edges: ");
+        for (Map.Entry<T,List<Edge<T>>> vertex:adjacencyLists.entrySet()){
+            stringBuilder.append( "Vertex: "+vertex.getKey().toString()+" Edges: ");
             for(Edge edge:vertex.getValue())
                 stringBuilder.append(edge.destination.value+" ");
             stringBuilder.append("\n");
@@ -81,4 +81,24 @@ public class Graph<T extends Comparable<? super T>> implements Comparable<Graph<
         }
         return stringBuilder.toString();
     }
+
+    public List<T> bfs(T vertex){
+        List<T> result = new ArrayList<>();
+        boolean[] visited = new boolean[numberOfVertices];
+        Queue<T> queue = new LinkedList<>();
+        queue.offer(vertex);
+        visited[(Integer) vertex]=true;
+        while (!queue.isEmpty()){
+            T temp =queue.poll();
+            result.add(temp);
+            for (Edge<T> edge: adjacencyLists.get(temp)){
+                if(!visited[(Integer) edge.destination.value]){
+                    visited[(Integer) edge.destination.value]=true;
+                    queue.offer(edge.destination.value);
+                }
+            }
+        }
+        return result;
+    }
+
 }
